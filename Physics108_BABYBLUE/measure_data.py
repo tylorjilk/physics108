45812067e6b1f537ed5e2fb49f49a10a30fed66f
Tylor Jilk <tjilk2663@gmail.com>
2019-05-03 13:04:28 -0700

    updating files

--- Physics108_BABYBLUE/measure_data.py
+++ Physics108_BABYBLUE/measure_data.py
@@ -3,8 +3,9 @@ import time
 import sys
 
 enable_22 = False	# Whether or not the nanovolt meter is connected
-enable_2 = True		# Whether or not the mod coil multimeter is connected
-enable_3 = True		# Whether or not the squid pickup multimeter is connected
+enable_2 = False	# Whether or not the mod coil multimeter is connected
+enable_3 = True	# Whether or not the squid pickup multimeter is connected
+enable_4 = True		# Whether or not the temp probe multimeter is connected
 mod_sense_resistor = 101.0 # Value of mod coil sense resistor in ohms
 squid_sense_resistor = 101.0 # Value of squid pickup sense resister in ohms
 
@@ -12,6 +13,7 @@ squid_sense_resistor = 101.0 # Value of squid pickup sense resister in ohms
 nanovolt_22_address = 'GPIB0::22::INSTR'
 multimeter_2_address = 'GPIB0::2::INSTR'
 multimeter_3_address = 'GPIB0::3::INSTR'
+multimeter_4_address = 'GPIB0::4::INSTR'
 
 def main():
 	rm = visa.ResourceManager()
@@ -20,7 +22,7 @@ def main():
 	# Make a new log file with time appended to name
 	init_time = time.localtime()
 	timestamp = str(init_time.tm_hour) + str(init_time.tm_min) + str(init_time.tm_sec)
-	file = open("C:/Users/Student/physics108/Physics108_BABYBLUE/data/testdata" + timestamp + ".txt", "w")
+	file = open("C:/Users/Student/physics108/Physics108_BABYBLUE/data/diode_4_terminal_test" + timestamp + ".txt", "w")
 
 	column_label = 'Time (s)'
 
@@ -37,6 +39,10 @@ def main():
 		multimeter_3 = rm.open_resource(multimeter_3_address)
 		column_label += ',SQUID Loop Voltage (V)'
 		column_label += ',Calculated Squid Current (uA)'
+		
+	if enable_4:
+		multimeter_4 = rm.open_resource(multimeter_4_address)
+		column_label += ',Temp Probe Voltage (V)'
 
 	# Write column labels in the log file
 	print(column_label)
@@ -57,19 +63,23 @@ def main():
 			multi_2_voltage = multimeter_2.query('MEAS?')
 			data_string += ',' + str(multi_2_voltage.strip('\n'))
 			multi_2_current = float(multi_2_voltage) / mod_sense_resistor * 1E6
-			# sys.stdout.write('Mod current (uA): ' + str(multi_2_current) + '\r')
 			data_string += ',' + str(multi_2_current)
 			
 		if enable_3:
 			multi_3_voltage = multimeter_3.query('MEAS?')
 			data_string += ',' + str(multi_3_voltage.strip('\n'))
 			multi_3_current = float(multi_3_voltage) / squid_sense_resistor * 1E6
-			#sys.stdout.write('Squid Current (uA): ' + str(multi_3_current) + '\r')
 			data_string += ',' + str(multi_3_current)
+		
+		if enable_4:
+			multi_4_voltage = multimeter_4.query('MEAS?')
+			data_string += ',' + str(multi_4_voltage.strip('\n'))
+		
 		sys.stdout.write(data_string)
 		sys.stdout.write('\r')
 		sys.stdout.flush()
 		data_string += '\n'
+		
 		file.write(data_string)
 	
 if __name__ == "__main__":
