8aaf6be563d206425f4d4a53fa489a99f4b18622
horraywwkd <horray@stanford.edu>
2019-05-12 17:12:26 -0700

    Add files via upload

--- Physics108_BABYBLUE/graph.py
+++ Physics108_BABYBLUE/graph.py
@@ -4,31 +4,51 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import sys
+
+def sincurve(x, A, T, phi, b):
+    return A*np.sin(2*np.pi/T*x+phi)+b
 
 def main():
 	df = pd.read_csv(sys.argv[1])
-	time = df['Time (s)']
-	counter = df['Counter']
-	squid_voltage = df['SQUID voltage (V)']	
-	mod_current = df['Calculated Mod Coil Current (uA)']	
-	squid_loop_current = df['Calculated Squid Current (uA)']
-	temp_voltage = df['Temp Probe Voltage (V)']
-
-
-
-	#squid_voltage = df['SQUID voltage (V)']
-	#mod_voltage = df['Mod Coil Voltage (V)']
-	#mod_current = df['Calculated Mod Coil Current (uA)']
-	#squid_loop_voltage = df['SQUID Loop Voltage (V)']
-	#squid_loop_current = df['Calculated Squid Current (uA)']
-	#temp_voltage = df['Temp Probe Voltage (V)']
+	if len(sys.argv > 2):
+        option = sys.argv[2]
+        
+	counter = df['Counter,']
+	time = df['Time(s),']
+	squid_voltage = df['SQUID Voltage(V),']
+	mod_current = df['Mod Coil Current(uA),']
+	pickup_loop_current = df['Pickup Loop Current(uA),']
+	temp_voltage = df['Temp Probe Voltage(V),']
+	field_coil_current = df['Field Coil Current(mA),']
+	magnet_current = df['Magnet Current(mA)']
+    
+	squid_voltage = np.absolute(np.array(squid_voltage))
+	mod_current = np.absolute(np.array(mod_current))
 	
-	plt.plot(mod_current, squid_voltage, 'bo')
+	plt.plot(mod_current, squid_voltage, '.')
 	plt.xlabel('Mod Current (uA)')
 	plt.ylabel('SQUID Voltage (V)')
-	squid_avg_current = "{:.2E}".format(np.average(squid_loop_current))
-	plt.title('Squid voltage vs mod current at ' + squid_avg_current + ' uA')
+	mean_pickup_loop_current = "{:.2E}".format(np.average(pickup_loop_current))
+    mean_temp_voltage = "{:.4E}".format(np.average(temp_voltage))
+	plt.title('Squid Voltage vs Mod Current at pickup loop current ' \
+           + mean_pickup_loop_current + 'uA, temperature voltage ' + mean_temp_voltage +'V')
+    
+    x_axis = np.linspace(0, np.max(mod_current))
+    if option=='fit':
+        coeff, covar = curve_fit(sincurve, mod_current, squid_voltage, p0=[4, 140, 0, 0])
+        plt.plot(x_axis, sincurve(x_axis, coeff) ,'-')
+        
+        sensitivity = coeff[0]*2*np.pi/coeff[1]/(14E-12)*2.07E-15*1E6  #uV per flux quantum
+        sweetspot = -coeff[2]*coeff[1]/(2*np.pi)
+        while sweetspot < 0:
+            sweetspot += coeff[1]
+        while sweetspot > coeff[1]:
+            sweetspot -= coeff[1]
+            
+        print('Sweetspot = '+str(sweetspot)+'uA '+'Max Sensitivity = '+str(sensitivity)+'uV/Phi0')
+    
 	plt.show()
 
 if __name__ == "__main__":
--- Physics108_BABYBLUE/measure_data.py
+++ Physics108_BABYBLUE/measure_data.py
@@ -10,11 +10,15 @@ enable_3 = True					# Whether or not the squid pickup multimeter is connected
 enable_4 = True					# Whether or not the temp probe multimeter is connected
 mod_sense_resistor = 101.5 		# Value of mod coil sense resistor in ohms
 squid_sense_resistor = 101.5 	# Value of squid pickup sense resister in ohms
+fc_sense_resistor = ?
+magnet_sense_resistor = ?
 
 nanovolt_22_address = 'GPIB0::22::INSTR'	# nanovolt meter
 multimeter_2_address = 'GPIB0::2::INSTR'	# mod coil multimeter
 multimeter_3_address = 'GPIB0::3::INSTR'	# squid pickup multimeter
 multimeter_4_address = 'GPIB0::4::INSTR'	# temp probe multimeter
+multimeter_?_address = 'GPIB0::?::INSTR'    # field coil multimeter
+multimeter_??_address = 'GPIB0::??::INSTR'  # magnet multimeter
 
 
 
@@ -27,26 +31,34 @@ def main():
 	timestamp = str(init_time.tm_hour) + str(init_time.tm_min) + str(init_time.tm_sec)
 	file = open("C:/Users/Student/physics108/Physics108_BABYBLUE/data/" + identifier + timestamp + ".txt", "w")
 
-	column_label = 'Time (s) '
-	column_label += ', Counter '
+	column_label = 'Counter,'
+	column_label += 'Time(s),'
 
 	if enable_22:
 		nanovolt_meter_22 = rm.open_resource(nanovolt_22_address)
-		column_label += ', SQUID voltage (V) '
+		column_label += 'SQUID Voltage(uV),'
 
 	if enable_2:
 		multimeter_2 = rm.open_resource(multimeter_2_address)
 		#column_label += ', Mod Coil Voltage (V) '
-		column_label += ', Calculated Mod Coil Current (uA) '
+		column_label += 'Mod Coil Current(uA),'
 	
 	if enable_3:
 		multimeter_3 = rm.open_resource(multimeter_3_address)
 		#column_label += ', SQUID Loop Voltage (V) '
-		column_label += ', Calculated Squid Current (uA) '
+		column_label += 'Pickup Loop Current(uA),'
 		
 	if enable_4:
 		multimeter_4 = rm.open_resource(multimeter_4_address)
-		column_label += ', Temp Probe Voltage (V) '
+		column_label += 'Temp Probe Voltage(V),'
+        
+ 	if enable_?:
+		multimeter_? = rm.open_resource(multimeter_?_address)
+		column_label += 'Field Coil Current(mA),'  
+        
+ 	if enable_??:
+		multimeter_?? = rm.open_resource(multimeter_??_address)
+		column_label += 'Magnet Current(mA)'          
 
 	# Write column labels in the log file
 	print(column_label)
@@ -57,29 +69,40 @@ def main():
 
 	counter = 0
 	while(True):
+		data_string = str(counter)        
 		current_time = time.time() - start_time
-		data_string = str(current_time)
-		data_string += ' , '+str(counter)
+		data_string += ','+str(current_time)      
 		
 		if enable_22:
 			nano_22_voltage = nanovolt_meter_22.query('MEAS:DIFF?')
-			data_string += ' , ' + str(nano_22_voltage.strip('\n'))
+			nano_22_voltage = float(nano_22_voltage) * 1E6            
+			data_string += ',' + str(nano_22_voltage.strip('\n'))
 
 		if enable_2:
 			multi_2_voltage = multimeter_2.query('MEAS?')
-			#data_string += ' , ' + str(multi_2_voltage.strip('\n'))
+			#data_string += ',' + str(multi_2_voltage.strip('\n'))
 			multi_2_current = float(multi_2_voltage) / mod_sense_resistor * 1E6
-			data_string += ' , ' + str(multi_2_current)
+			data_string += ',' + str(multi_2_current)
 			
 		if enable_3:
 			multi_3_voltage = multimeter_3.query('MEAS?')
-			#data_string += ' , ' + str(multi_3_voltage.strip('\n'))
+			#data_string += ',' + str(multi_3_voltage.strip('\n'))
 			multi_3_current = float(multi_3_voltage) / squid_sense_resistor * 1E6
-			data_string += ' , ' + str(multi_3_current)
+			data_string += ',' + str(multi_3_current)
 		
 		if enable_4:
 			multi_4_voltage = multimeter_4.query('MEAS?')
-			data_string += ' , ' + str(multi_4_voltage.strip('\n'))
+			data_string += ',' + str(multi_4_voltage.strip('\n'))
+            
+		if enable_?:
+			multi_?_voltage = multimeter_?.query('MEAS?')
+			multi_?_current = float(multi_?_voltage) / fc_sense_resistor * 1E3        
+			data_string += ',' + str(multi_?_current.strip('\n'))    
+            
+		if enable_??:
+			multi_??_voltage = multimeter_??.query('MEAS?')
+			multi_??_current = float(multi_??_voltage) / magnet_sense_resistor * 1E3        
+			data_string += ',' + str(multi_??_current.strip('\n'))                
 		
 		sys.stdout.write(data_string)
 		sys.stdout.write('\r\r')
