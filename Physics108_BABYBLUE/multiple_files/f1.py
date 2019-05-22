683b0ada32f867d92df0e5980b1206c8fde81aba
Tylor Jilk <tjilk2663@gmail.com>
2019-05-22 02:11:59 -0700

    updating

--- Physics108_BABYBLUE/multiple_files/f1.py
+++ Physics108_BABYBLUE/multiple_files/f1.py
@@ -1,29 +1,92 @@
 import physics108 as p108
+import pandas as pd
 import visa
+import time
+import signal
 import sys
 
+# Capture Ctl-C, Save Data, and Clear Devices
+def signal_handler(sig, frame):
+	print('Resetting devices...')
+	clear_devices()
+	print('Saving data...')
+	save_data()
+	print('Goodbye')
+	sys.exit(0)
+
 def main():
-	identifier = raw_input("Identifier for the run? ")
-	
+	p108.RUN_IDENTIFIER = str(raw_input("Identifier for this run?  "))
+	signal.signal(signal.SIGINT, signal_handler)
 	rm = visa.ResourceManager()
 	
-	# Syntax is Device('address', visa.ResourceManager(), Enabled?)
-	nanovolt_meter = p108.Device('GPIB0::22::INSTR', rm, True)
+	global nanovoltmeter
+	global multimeter_squid_curr_sense
+	global multimeter_temperature
+	global multimeter_mod_curr_sense
+	global fngen_mod_curr_source
+	global fngen_fieldcoil_curr_trig
+	global magnet_programmer
 	
-	fieldcoil_fnGen = p108.Device('GPIB0::7::INSTR', rm, True)
-	fieldcoil_currMeas = p108.Device('GPIB0::3::INSTR', rm, True)
-	fieldcoil_currMeas.setResValue(50.0)
+	nanovoltmeter					= p108.Device( p108.NANOVOLTMETER_ADDRESS, 			rm, 	True)
+	multimeter_squid_curr_sense		= p108.Device( p108.SQUID_CURRENT_SENSE_ADDRESS, 	rm, 	False)
+	multimeter_temperature			= p108.Device( p108.TEMPERATURE_ADDRESS, 			rm, 	True)
+	multimeter_mod_curr_sense 		= p108.Device( p108.MOD_CURRENT_SENSE_ADDRESS,	 	rm, 	False)
+	fngen_mod_curr_source			= p108.Device( p108.MOD_CURRENT_SOURCE_ADDRESS,		rm, 	False)
+	fngen_fieldcoil_curr_trig		= p108.Device( p108.FIELDCOIL_CURRENT_TRIG_ADDRESS,	rm, 	False)
+	magnet_programmer				= p108.Device( p108.MAGNET_PROG_ADDRESS,			rm, 	False)
 	
-	magnet_prog = p108.Device('GPIB0::5::INSTR', rm, True)
-	squid_currMeas = p108.Devices('GPIB0::6::INSTR', rm, True) # Not the right address!!!!!!!!!!!!!!!!!!!!!!
-	#squid_currMeas.setResValue(??)
+	global device_list
+	device_list = [	nanovoltmeter, 
+					multimeter_squid_curr_sense, 
+					multimeter_temperature,
+					multimeter_mod_curr_sense,
+					fngen_mod_curr_source,
+					fngen_fieldcoil_curr_trig]
 	
-	modcoil_fnGen = p108.Device('GPIB0::6::INSTR', rm, True)
-	modcoil_currMeas = p108.Device('GPIB0::6::INSTR', rm, True)
-	#modcoil_currMeas.setResValue(??)
+	configure_devices()
+	configure_data_constants()
 	
-	
-	
+	while(True):
+		nanovoltmeter.init()
+		multimeter_temperature.init()
+		time.sleep(10)
+		print("Nanovolt data:")
+		print(nanovoltmeter.fetc_data())
+		print("Temperature data:")
+		print(multimeter_temperature.fetc_data())
 
+def configure_devices():
+	multimeter_temperature.write('CONF:VOLT:DC')
+	multimeter_temperature.write('TRIG:SOUR EXT')
+	multimeter_temperature.write('TRIG:COUN 50')
+	
+	nanovoltmeter.write('CONF:VOLT:DC:DIFF')
+	nanovoltmeter.write('SENS:VOLT:DC:NPLC 2')
+	nanovoltmeter.write('TRIG:SOUR EXT')
+	nanovoltmeter.write('TRIG:COUN 50')
+	nanovoltmeter.write('TRIG:DEL 0')
+
+def save_data():
+	# Make a new log file with time appended to name
+	init_time = time.localtime()
+	timestamp = str(init_time.tm_hour) + str(init_time.tm_min) + str(init_time.tm_sec)
+	filename = "C:/Users/Student/physics108/Physics108_BABYBLUE/testdata/" + str(p108.RUN_IDENTIFIER) + "_" + timestamp + ".txt"
+	file = open(filename, "w")
+	
+	datalist = []
+	for device in device_list:
+		if device.isEnabled():
+			data = device.get_complete_data()
+			datalist.append(data)
+	df = pd.DataFrame({	'A': datalist[0],
+						'B': datalist[1]	})
+	df.to_csv(filename)
+
+def clear_devices():
+	for device in device_list:
+		if device.isEnabled():
+			device.write('*CLS')
+			device.write('*RST')
+		
 if __name__ == "__main__":
     main()
--- Physics108_BABYBLUE/multiple_files/physics108.py
+++ Physics108_BABYBLUE/multiple_files/physics108.py
@@ -1,8 +1,36 @@
+"""
+List of constants
+"""
+NANOVOLTMETER_ADDRESS			= 'GPIB0::22::INSTR'
+SQUID_CURRENT_SENSE_ADDRESS		= 'GPIB0::4::INSTR'
+TEMPERATURE_ADDRESS				= 'GPIB0::2::INSTR'
+MOD_CURRENT_SENSE_ADDRESS		= 'GPIB0::9::INSTR'
+MOD_CURRENT_SOURCE_ADDRESS		= 'GPIB0::8::INSTR'
+FIELDCOIL_CURRENT_TRIG_ADDRESS	= 'GPIB0::7::INSTR'
+MAGNET_PROG_ADDRESS				= 'GPIB0::5::INSTR'
+
+RUN_IDENTIFIER = ''
+
+MAGNET_FIELD_CONSTANT = 0.10238 	#field constant of magnet, T/A
+MAGNET_FIELD_RAMP_RATE = 0.03		# ramp rate of magnet, T/s
+MAGNET_CURRENT_RAMP_RATE = 0.3 		# A/s  magnet_ramp_rate/magnet_constant*0.1
+MAGNET_MAX_CURRENT = 1.5 			# A
+
+FIELDCOIL_SENSE_RESISTOR = 50.0		# ohms
+MOD_SENSE_RESISTOR = 9.13E3			# ohms
+MOD_BFR_RESISTOR = 15.0E3			# ohms
+SQUID_CURR_SENSE_RESISTOR = 101.5	# ohms
+
+
+"""
+Device Class
+"""
 class Device:
 	def __init__(self, address, resourceManager, en):
 		self.address = address
 		self.en = en
 		self.rm = resourceManager
+		self.data = []
 		if self.en:
 			self.connect()
 	
@@ -18,6 +46,8 @@ class Device:
 	def connect(self): # Opens the pyvisa comms pathway to device
 		if self.en:
 			self.resource = self.rm.open_resource(self.address)
+			self.write('*CLS')
+			self.write('*RST')
 	
 	def setResValue(self, res): # Sets the sense resistor value, in ohms
 		self.resistorValue = res
@@ -31,4 +61,13 @@ class Device:
 	def query(self, message): # Queries a message to the device and returns the response
 		return self.resource.query(message)
 	
-class 
+	def init(self):
+		self.write('INIT')
+	
+	def fetc_data(self):
+		newdata = (self.resource.query('FETC?').strip('\n')).split(',')
+		self.data.extend(newdata)
+		return newdata
+	
+	def get_complete_data(self):
+		return self.data
