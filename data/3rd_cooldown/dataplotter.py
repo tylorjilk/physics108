d2c4bdca291db434691550e7ff994cde1bb04445
Tylor Jilk <tjilk2663@gmail.com>
2019-05-27 16:54:13 -0700

    3rdcooldownfinal

--- data/3rd_cooldown/dataplotter.py
+++ data/3rd_cooldown/dataplotter.py
@@ -18,7 +18,7 @@ times = np.array(pd.DataFrame(data, columns= ['timestamp']))
 pickup_voltages = np.absolute(np.array(pd.DataFrame(data, columns= ['SQUID_Volt(V)']))*1E6)
 pickup_voltages = pickup_voltages[~pd.isnull(pickup_voltages)]
 
-plt.plot(times[5:505], pickup_voltages[5:505], '.')
+plt.plot(times[5:], pickup_voltages[5:], '.')
 plt.xlabel('Time(s)')
 plt.ylabel('SQUID Voltage(uV)')
 plt.show()
